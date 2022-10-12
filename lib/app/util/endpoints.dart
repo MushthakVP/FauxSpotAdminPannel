9b853dd00e80a7d1367c79323fe21b3d9abe32a0
Mushthak <100484745+MushthakVP@users.noreply.github.com>
2022-10-12 08:44:05 +0530

    Update endpoints.dart

--- lib/app/util/endpoints.dart
+++ lib/app/util/endpoints.dart
@@ -1,7 +1,7 @@
 class EndPoints {
   static String baseUrl = "https://fauxspot.herokuapp.com/";
   static String webGetUrl = "${baseUrl}user/all-turf";
-  static String webSignupOtp = "${baseUrl}account/login-number";
+  static String webSignupOtp = "${baseUrl}account/loginwith-number";
   static String webOtpverify = "${baseUrl}account/verify-number-otp";
   static String vendorTurf = "${baseUrl}user/vendor-turf/{vendorid}";
   static String updateTurf = "${baseUrl}vendor/updateturf/{turfid}";
